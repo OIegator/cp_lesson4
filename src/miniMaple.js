b4813df3e20b55cb4e0d36616a23ee9b572ba0bd
OIegator <64355448+OIegator@users.noreply.github.com>
2022-09-27 17:23:33 +0300

    Add unit divider and tests

--- src/miniMaple.js
+++ src/miniMaple.js
@@ -1,5 +1,41 @@
 class MiniMaple{
+    constructor(string, x){
+        this.units = new Map()
+		this.string = string
+		this.x = x
+		devide_into_units(this.string)
+    }
+	
+	devide_into_units(str){
+		let csign = true
+		let buff = ''
+		for(let i = 0; i < str.length, i++) {
+			switch(str[i]) {
+			  case '-':
+			  if(buff) {
+				this.units.set(buff, csign)
+				buff = ''  
+			  }
+				csign = false
+				break
 
+			  case '+': 
+			  if(buff) {
+				this.units.set(buff, csign)
+				buff = ''
+			  }
+				csign = true
+				break
+			
+			  default:
+				buff += str[i]
+			}
+		}
+	}
+	
+	calc() {
+		
+	}
 }
 
 export {MiniMaple}
--- test/miniMaple.test.js
+++ test/miniMaple.test.js
@@ -1,5 +1,26 @@
 import {MiniMaple} from "../src/miniMaple";
 
-test('it fails', () => {
-    expect(false).toBeTruthy();
+test('Work for a single unit', () => {
+	let mm = new MiniMaple('4*x^3', 'x')
+    expect(mm.calc()).toBe('12*x^2');
+});
+
+test('Work for a several units', () => {
+	let mm = new MiniMaple('4*x^3-x^2', 'x')
+    expect(mm.calc()).toBe('12*x^2 - 2*x');
+});
+
+test('Returns empty string, when no units attached to this varable', () => {
+	let mm = new MiniMaple('4*x^3', 'y')
+    expect(mm.calc()).toBe('');
+});
+
+test('Returns empty string from empty string', () => {
+	let mm = new MiniMaple('', 'x')
+    expect(mm.calc()).toBe('');
+});
+
+test('Not loosing negative on first unit', () => {
+	let mm = new MiniMaple('-4*x^3-x^2', 'x')
+    expect(mm.calc()).toBe('-12*x^2 - 2*x');
 });
