e2043aa9104269fc5ffc9f821ea0ba05aa5adf37
OIegator <olegshev506@gmail.com>
2022-09-30 17:40:53 +0300

    Add calc() and some tests

--- src/miniMaple.js
+++ src/miniMaple.js
@@ -3,38 +3,95 @@ class MiniMaple{
         this.units = new Map()
 		this.string = string
 		this.x = x
-		devide_into_units(this.string)
+		this.divide_into_units(this.string)
     }
-	
-	devide_into_units(str){
-		let csign = true
-		let buff = ''
-		for(let i = 0; i < str.length, i++) {
-			switch(str[i]) {
-			  case '-':
-			  if(buff) {
-				this.units.set(buff, csign)
-				buff = ''  
-			  }
-				csign = false
-				break
 
-			  case '+': 
-			  if(buff) {
-				this.units.set(buff, csign)
-				buff = ''
-			  }
-				csign = true
-				break
-			
-			  default:
-				buff += str[i]
+	divide_into_units(str){
+		let cur_sign = true
+		let buff = ""
+		for(let i = 0; i < str.length; i++) {
+			switch (str[i]) {
+				case '-':
+					if (buff) {
+						this.units.set(buff, cur_sign)
+						buff = ""
+					}
+					cur_sign = false
+					break
+
+				case '+':
+					if (buff) {
+						this.units.set(buff, cur_sign)
+						buff = ""
+					}
+					cur_sign = true
+					break
+
+				default:
+					buff += str[i]
 			}
 		}
+		this.units.set(buff, cur_sign)
 	}
-	
+
 	calc() {
-		
+		let res = ""
+		this.units.forEach((value, key) => {
+			let num = 1
+			let cur_res = ""
+			let cur_num = ""
+			let after_x = false
+			let pow_x = 0
+			let pow_any = false
+			if(key.includes(this.x)) {
+				for(let i = 0; i < key.length; i++) {
+					switch(key[i]) {
+						case this.x:
+							after_x = true
+							break
+						case '^':
+							if (after_x) pow_x = 1
+							else {
+								cur_res += "^"
+								pow_any = true
+							}
+							break
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+						case '5':
+						case '6':
+						case '7':
+						case '8':
+						case '9':
+							if (i !== key.length && !isNaN(key[i + 1]) ) {
+								cur_num += key[i]
+							} else if (pow_any) {
+								cur_res += cur_num + key[i]
+							} else {
+								num *= Number(cur_num + key[i])
+								if (pow_x) cur_res += this.x + "^" + (Number(cur_num + key[i]) - 1)
+								cur_num = ""
+								//console.log(num)
+							}
+							break
+						default:
+							after_x = false
+							cur_res += key[i]
+
+					}
+
+				}
+			}
+			console.log(cur_res)
+			if(!value) res += "-"
+			else if(res !== "") res += "+"
+			res += num + cur_res
+		})
+		console.log(res.replace(/-1\*/g, "-").replace(/\+1\*/g, "-"))
+		return (res === "1") ? "" : res.replace(/-1\*/g, "-").replace(/\+1\*/g, "-")
 	}
 }
 
--- test/miniMaple.test.js
+++ test/miniMaple.test.js
@@ -7,10 +7,10 @@ test('Work for a single unit', () => {
 
 test('Work for a several units', () => {
 	let mm = new MiniMaple('4*x^3-x^2', 'x')
-    expect(mm.calc()).toBe('12*x^2 - 2*x');
+    expect(mm.calc()).toBe('12*x^2-2*x');
 });
 
-test('Returns empty string, when no units attached to this varable', () => {
+test('Returns empty string, when no units attached to this variable', () => {
 	let mm = new MiniMaple('4*x^3', 'y')
     expect(mm.calc()).toBe('');
 });
@@ -22,5 +22,22 @@ test('Returns empty string from empty string', () => {
 
 test('Not loosing negative on first unit', () => {
 	let mm = new MiniMaple('-4*x^3-x^2', 'x')
-    expect(mm.calc()).toBe('-12*x^2 - 2*x');
+    expect(mm.calc()).toBe('-12*x^2-2*x');
 });
+
+test('Works for function with several variable', () => {
+    let mm = new MiniMaple('-4*y*x^3-x*y^2', 'x')
+    expect(mm.calc()).toBe('-12*y*x^2-y^2');
+});
+
+
+test('Works correctly for two - digit numbers', () => {
+    let mm = new MiniMaple('-10*x', 'x')
+    expect(mm.calc()).toBe('-10');
+});
+
+test('Works correctly for two - digit power', () => {
+    let mm = new MiniMaple('x^10', 'x')
+    expect(mm.calc()).toBe('10x^9');
+});
+
